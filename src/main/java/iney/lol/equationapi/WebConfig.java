3bae4b491ddeedbd4c98cc9a294fa425503776cd
Egor <33691073+iNeydlis@users.noreply.github.com>
2025-02-20 00:05:58 +0300

    feat: add developer credits with GitHub link

--- src/main/java/iney/lol/equationapi/WebConfig.java
+++ src/main/java/iney/lol/equationapi/WebConfig.java
@@ -10,7 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                        "http://iney.lol:3000",
                         "http://localhost:3000"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
